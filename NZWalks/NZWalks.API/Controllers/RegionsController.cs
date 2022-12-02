3bfa826ee5c35add936b651dcef8adc2381e91e7
121138 <121138@IN-500161>
2022-12-02 20:37:13 +0530

    End of Section 3 - Added RegionsController, Repository  Pattern, DTO's, AutoMapper, Async Call

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAllAsync();
+
+            //Return the data from the DTO(Data Transfer Object) instead of Domain(DB)
+            //var regionsDTO = new List<RegionDTO>();
+
+            //regions.ToList().ForEach(regionDB =>
+            //{
+            //    var regionDTO = new RegionDTO()
+            //    {
+            //        Id = regionDB.Id,
+            //        Code = regionDB.Code,
+            //        Name = regionDB.Name,
+            //        Area = regionDB.Area,
+            //        Lat = regionDB.Lat,
+            //        Long = regionDB.Long,
+            //        Population = regionDB.Population
+            //    };
+            //    regionsDTO.Add(regionDTO);
+            //});
+
+            //Using AutoMapper
+
+            var regionsDTO = mapper.Map<List<RegionDTO>>(regions); // mapper.Map<List<Destination>>(Source);
+            return Ok(regionsDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Profiles/RegionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionProfile : Profile
+    {
+        public RegionProfile()
+        {
+            CreateMap<Region, RegionDTO>(); //Source - (Region), Destination - (RegionDTO)
+        }
+    }
+}
